ee4721a50296e5f25e8f9b7f75683a0c17ad0424
Michael Seddon <michael.seddon@cliffordchance.com>
2019-03-15 15:41:19 +0000

    BTM cleanup

--- script/topicDisplay.py
+++ script/topicDisplay.py
@@ -27,6 +27,7 @@ def dispTopics(pt, voca, pz, filename):
         wvs = zip(range(len(vs)), vs)
         wvs = sorted(wvs, key=lambda d:d[1], reverse=True)
         tmps = ' '.join(['%s' % voca[w] for w,v in wvs[:10]])
+        #uncomment line below to print the individual word probability
         #tmps = ' '.join(['%s:%f' % (voca[w],v) for w,v in wvs[:10]])
         topics.append((pz[k], tmps))
         k += 1
